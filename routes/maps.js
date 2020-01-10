87b7541f02a5c1eafb86dae8a2c754c179e469d9
Aaron <aaronpiolang@gmail.com>
2020-01-10 21:19:49 +0000

    Created maps route, and updated server.js

--- routes/maps.js
+++ routes/maps.js
@@ -5,22 +5,20 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
-module.exports = (db) => {
+module.exports = db => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
+    let query = `SELECT * FROM maps`;
     console.log(query);
     db.query(query)
       .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
+        const maps = data.rows;
+        res.json({ maps });
       })
       .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+        res.status(500).json({ error: err.message });
       });
   });
   return router;
