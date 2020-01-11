c6452cd7d90bfcb53f2eb40335e5f356f675b44a
Aaron <aaronpiolang@gmail.com>
2020-01-11 17:21:52 +0000

    removed double semicolon

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,9 +3,11 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(users => {
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
     }
-  });;
+  });
 });
