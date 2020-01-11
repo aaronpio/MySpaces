4b0126546e745715fbb4b04035b9f399f5293443
Aaron <aaronpiolang@gmail.com>
2020-01-11 02:16:31 +0000

    Finished getUserById query functionality

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const dbParams = require("./lib/db.js");
 const db = new Pool(dbParams);
 db.connect();
 
+exports.db = db;
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -35,12 +36,12 @@ app.use(
 );
 app.use(express.static("public"));
 
-
-exports.getQueryResults = async (sql) => {
-  return db.query(sql)
-           .then(res => res.rows)
-           .catch(err => console.log(err));
-}
+exports.getQueryResults = async sql => {
+  return db
+    .query(sql)
+    .then(res => res.rows)
+    .catch(err => console.log(err));
+};
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
@@ -49,7 +50,7 @@ const mapsRoutes = require("./routes/maps");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
+app.use("/api/users", usersRoutes);
 app.use("/api/maps", mapsRoutes);
 // Note: mount other resources here, using the same pattern above
 
@@ -63,4 +64,3 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
