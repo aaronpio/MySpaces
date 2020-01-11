faca227789ea2fb930f77a936a7ee464c729b26b
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-10 19:39:26 -0500

    Switch back to EJS - Eat my cabbage Francis!!

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ db.connect();
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   "/styles",
