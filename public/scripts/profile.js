5acabc013d05c6607e5b571d6846f18f0a08936c
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-13 16:19:29 -0500

    Fetch maps for profile with jQuery

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -1,3 +1,14 @@
 $(() => {
-
+  const userID = /user-id=(\d+)/.exec(document.cookie)[1];
+  $.ajax({
+    url: `/users/${userID}/maps`
+  }).done(res => {
+    const maps = JSON.parse(res);
+    const $maps = maps.map(map => {
+      $(`<li>
+        <a href="/maps/${map.id}">${map.name}</a>
+      </li>`)
+    })
+    $("#my-maps").append($maps);
+  })
 })
