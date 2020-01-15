dc30dc358f9dfecfdfce26f7d798ed317309455e
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-15 16:16:17 -0500

    Fix favorite button class toggling

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,37 +3,35 @@ $(() => {
 
   $.ajax({ url: "/api/maps" }).done(maps => {
     for (map of maps) {
-      const $favoriteButton = $(`
-       <a href="#" class="btn btn-outline-warning">Favorite</a>`)
+      const $favBtn = $(`
+       <a href="#" class="btn btn-outline-warning">Add to favorites</a>`);
 
-      $favoriteButton.click(e => {
+      $favBtn.click(e => {
         // e.preventDefault()
         // const mapID = $card.data("id");
         $.ajax({
           url: `/api/favorites/${map.id}/toggle`,
           method: "POST"
         }).done(() => {
-          const $card = $(this).closest(".card");
-          if ($(this).hasClass(".btn-outline-warning")) {
-            $(this).removeClass(".btn-outline-warning")
-            $(this).addClass(".btn-outline-danger")
-          } else {
-            $(this).removeClass(".btn-outline-danger")
-            $(this).addClass(".btn-outline-warning")
-          }
-        })
-      })
+          $favBtn.toggleClass("btn-outline-warning");
+          $favBtn.toggleClass("btn-outline-danger");
+          $favBtn.hasClass("btn-outline-warning")
+            ? $favBtn.text("Add to favorites")
+            : $favBtn.text("Remove from favorites");
+        });
+      });
 
-      const $card = $(`<div -data-id="${map.id}" class="card" style="min-width: 45%">
-           <div class="card-body">
-             <h5 class="card-title"><a href=/maps/${map.id}>${map.name}</a></h5>
-             <p class="card-text">Created by
+      const $card = $(`
+        <div -data-id="${map.id}" class="card" style="min-width: 45%">
+          <div class="card-body">
+           <h5 class="card-title"><a href=/maps/${map.id}>${map.name}</a></h5>
+           <p class="card-text">Created by
                <a href=/users/${map.owner_id}>${map.owner}</a>
              </p>
-           </div>
-         </div>`)
+             </div>
+           </div>`);
 
-      if (loggedIn) $card.find(".card-body").append($favoriteButton)
+      if (loggedIn) $card.find(".card-body").append($favBtn);
       $card.appendTo($("main"));
     }
   });
