7e23d0fef6e3d487d8960753c2046b9949845b21
Vincent-Carrier <vincentcarrier15@gmail.com>
2020-01-15 12:18:42 -0500

    Implement grid layout for map cards

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,13 +1,10 @@
 $(() => {
   const loggedIn = /user-id/.exec(document.cookie);
 
-  $.ajax({
-    method: "GET",
-    url: "/api/maps"
-  }).done(maps => {
+  $.ajax({ url: "/api/maps" }).done(maps => {
     for (map of maps) {
-      const favoriteButton = loggedIn ? `<a href="/api/favorites/${map.id} class="btn btn-primary">Favorite</a>` : "";
-      $(`<div class="card">
+      const favoriteButton = loggedIn ? `<a href="/api/favorites/${map.id}" class="btn btn-outline-warning">Favorite</a>` : "";
+      $(`<div class="card" style="min-width: 45%">
            <div class="card-body">
              <h5 class="card-title"><a href=/maps/${map.id}>${map.name}</a></h5>
              <p class="card-text">Created by <a href=/users/${map.owner_id}>${map.owner}</a></p>
@@ -18,5 +15,4 @@ $(() => {
     }
   });
 });
-
 
